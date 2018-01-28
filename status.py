bc3c7d85f33a9ef78dbf1b379b1fa489ff7c49a9
dtpowers <dontpowers@outlook.com>
2018-01-28 01:01:38 +0000

    db write / load works

--- status.py
+++ status.py
@@ -2,6 +2,7 @@ import boto3
 import datetime
 import io
 import json
+from time import sleep
 import pprint
 # from flask import Flask
 # app = Flask(__name__)
@@ -21,14 +22,16 @@ import pprint
 def serverInit():
     global DATABASE
     global prevStatus
-    global s3
     global pp
     pp = pprint.PrettyPrinter(indent=4)
-    s3 = boto3.client('s3')
     DATABASE = loadDB()
     print("Server initialized...")
     prevStatus = len(DATABASE)
     print('Previous Status #: ' + str(prevStatus))
+
+def shutdownServer():
+    saveDB()
+
 
 def recStatus(status):
     prevStatus += 1
@@ -49,16 +52,20 @@ def getStatus(id):
 #read database from disk into memory
 def loadDB():
     print('Loading Database...')
+    s3 = boto3.client('s3')
     response = s3.get_object(Bucket='pistatus', Key='data.json')
     database = json.loads(response['Body'].read().decode("UTF-8"))
     pp.pprint(database)
     DATABASE = database
     print('Finished Loading ')
+    return database
 
 
 #write db from memory to disk
 def saveDB():
-    s3.Bucket('pistatus').put_object(Key='data.json', Body=DATABASE)
+    buf = json.dumps(DATABASE).encode('UTF-8')
+    s3 = boto3.resource('s3')
+    s3.Bucket('pistatus').put_object(Key='data.json', Body=buf)
     print('Wrote DB to disk...')
 
 
@@ -66,3 +73,5 @@ def saveDB():
 ############################################## tests
 serverInit()
 loadDB()
+sleep(5)
+shutdownServer()
